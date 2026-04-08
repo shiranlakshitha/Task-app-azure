7a3f8fc27fb52d401603185fdd7bf8c8957829f4
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2026-04-08 09:54:24 +0530

    fix: update MongoDB connection URI variable from MONGODB_URI to MONGO_URI

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
